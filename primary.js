d244900c7869aa7c322d847a3cdff6afbc94eed1
Matt Tilley <mtilley255@gmail.com>
2018-02-28 13:20:21 -0600

    Deleted previous notes / continuing with practice.

--- primary.js
+++ primary.js
@@ -1,27 +1,3 @@
-/*  DID NOT REALIZE THIS!!!
-    Apparently the way you instantiate jQuery has changed
-    as of V3.
-    
-    Code below is the OLD WAY of doing it:
-    
-    $(document).ready(function()
-    {
-    
-    });
-    
-    
-    THIS IS NOW THE *NEW WAY* OF DOING IT...MUCH SIMPLER!!!
-    
-    $(function()
-    {
-    
-    });
-    
-    
-    
-*/
-
-
 $(function()
 {
     

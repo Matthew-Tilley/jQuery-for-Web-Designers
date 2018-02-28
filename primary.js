b998c5f082e5d7a1bf2cba3e604df83a1769c64c
Matt Tilley <mtilley255@gmail.com>
2018-02-28 14:11:18 -0600

    Revised code to make use of "chaining" per example.
    Way of DRYing your code; all methods operate upon the item selected.  Easier to read & understand / less code, etc.

--- primary.js
+++ primary.js
@@ -2,10 +2,10 @@ $(function()
 { "use strict";
     
  
-$("#TEST").css("background-color", "pink");
-$("#TEST").animate({height: "20vh"}, 2500);
-$("#TEST").fadeOut(5400);
-$("#TEST").fadeIn(10000);
+$("#TEST")  .css("background-color", "pink")
+            .animate({height: "20vh"}, 2500)
+            .fadeOut(5400)
+            .fadeIn(10000);
  
  
  

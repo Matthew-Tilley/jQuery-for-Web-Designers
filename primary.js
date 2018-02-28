31c397bb54495f45e8cb6dc41585b22ceda4aa7d
Matt Tilley <mtilley255@gmail.com>
2018-02-28 14:13:03 -0600

    Deleted practice code.

--- primary.js
+++ primary.js
@@ -2,12 +2,7 @@ $(function()
 { "use strict";
     
  
-$("#TEST")  .css("background-color", "pink")
-            .animate({height: "20vh"}, 2500)
-            .fadeOut(5400)
-            .fadeIn(10000);
- 
- 
+
  
  
  

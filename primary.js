3a0a8a7559e6e1806beec0415295c61636fd16b3
Matt Tilley <mtilley255@gmail.com>
2018-03-01 10:49:33 -0600

    Added items to follow along with lesson - works properly.

--- primary.js
+++ primary.js
@@ -4,6 +4,10 @@ $(function()
     $("#TESTER").on("click", function()
     {
         console.log("FIRST LINK ", $("a:first").attr("href"));
+        
+        $("a:first").attr("href", "https://www.yahoo.com");
+        
+        console.log("CHANGED LINK", $("a:first").attr("href"));
 
     });
   

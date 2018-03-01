2f375fbf79bb13d11dac39d30a35221fb4a22394
Matt Tilley <mtilley255@gmail.com>
2018-03-01 10:07:35 -0600

    Tinkering - trying something else / not working.

--- primary.js
+++ primary.js
@@ -2,11 +2,20 @@ $(function()
 {
     
     $("#TESTER").hover(BLURP);
+    $("#TESTER").mouseout(FLURP);
     
     
     function BLURP()
     {
         $(this).css("background-color", "yellow");
     };
+    
+    
+    function FLURP()
+    {
+        $(this).css("background-color", "none");
+    };
+    
+    
     
 });

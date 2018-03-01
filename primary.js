8802e07571e8ef575e5a49ae158732959e66692f
Matt Tilley <mtilley255@gmail.com>
2018-03-01 09:53:28 -0600

    Tried several different things...can't get his example to work.

--- primary.js
+++ primary.js
@@ -2,13 +2,12 @@ $(function()
 {
     
     
-    $("#TESTER").on("click", DOIT)
+    $("#TESTER").on("click", DOIT());
     {
         function DOIT()
         {
             console.log("LKAJLKJSAD");
         };
-    });
-    
+    });    
     
 });

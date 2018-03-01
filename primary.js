0d11ed3c9473584459dcbd3e5864846825254d53
Matt Tilley <mtilley255@gmail.com>
2018-03-01 10:53:11 -0600

    Cleared previous code for new lesson.

--- primary.js
+++ primary.js
@@ -1,15 +1,7 @@
 $(function()
 {
     
-    $("#TESTER").on("click", function()
-    {
-        console.log("FIRST LINK ", $("a:first").attr("href"));
-        
-        $("a:first").attr("href", "https://www.yahoo.com");
-        
-        console.log("CHANGED LINK", $("a:first").attr("href"));
-
-    });
+    
   
     
 });

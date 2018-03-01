3d3ed7cf8332adc3bae6f719da90e12aa8fc6173
Matt Tilley <mtilley255@gmail.com>
2018-03-01 10:10:14 -0600

    Deleted previous practice / tinker code.

--- primary.js
+++ primary.js
@@ -1,20 +1,6 @@
 $(function()
 {
     
-    $("#TESTER").hover(BLURP);
-    $("#TESTER").mouseout(FLURP);
-    
-    
-    function BLURP()
-    {
-        $(this).css("background-color", "yellow");
-    };
-    
-    
-    function FLURP()
-    {
-        $(this).css("background-color", "white");
-    };
     
     
     

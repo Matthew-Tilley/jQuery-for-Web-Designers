03bc83a52b83421b2b4f9eeec6a67f180d4a83c7
Matt Tilley <mtilley255@gmail.com>
2018-03-01 10:15:01 -0600

    Working the way I want now, but couldn't use one function.
    Thought perhaps I could use the "onmouseover()" method to accomplish the same thing; doesn't work like that.

--- primary.js
+++ primary.js
@@ -2,11 +2,18 @@ $(function()
 {
     
     $("#TESTER").mouseover(BONK);
+    $("#TESTER").mouseleave(BING);
     
     
     function BONK()
     {
         $(this).css("background-color", "purple");
+    };
+    
+    
+    function BING()
+    {
+        $(this).css("background-color", "white");
     };
     
     

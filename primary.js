e009bd34b1b602a9b6fd55603810a47cc83a179d
Matt Tilley <mtilley255@gmail.com>
2018-03-01 10:35:12 -0600

    Cleared previous code for next lesson.

--- primary.js
+++ primary.js
@@ -1,20 +1,6 @@
 $(function()
 {
     
-    $("#TESTER").mouseover(BONK);
-    $("#TESTER").mouseleave(BING);
-    
-    
-    function BONK()
-    {
-        $(this).css("background-color", "purple");
-    };
-    
-    
-    function BING()
-    {
-        $(this).css("background-color", "white");
-    };
-    
+  
     
 });

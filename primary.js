aed126127034f18474b4c9e0a96a2d3477e05fab
Matt Tilley <mtilley255@gmail.com>
2018-03-02 09:44:52 -0600

    Numerous (misc) changes to follow along with lesson / complete quiz.

--- primary.js
+++ primary.js
@@ -1,4 +1,14 @@
 $(function()
 {
-    $("a.gallery").colorbox( { rel : "gallery"} );
+    
+    $("a.gallery").colorbox( 
+    { 
+        rel : "gallery" ,
+        maxWidth : "70%",
+        slideshow : "true",
+        slideshowSpeed: "3000",
+        transition: "fade" ,
+        opacity: ".2"
+    });
+    
 });

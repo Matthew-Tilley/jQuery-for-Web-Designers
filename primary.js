330e3b91410766f566c757654a2e1bf60254c439
Matt Tilley <mtilley255@gmail.com>
2018-03-03 09:16:36 -0600

    Deleted practice code from previous lesson.

--- primary.js
+++ primary.js
@@ -1,14 +1,8 @@
 $(function()
 {
+ 
     
-    $("a.gallery").colorbox( 
-    { 
-        rel : "gallery" ,
-        maxWidth : "70%",
-        slideshow : "true",
-        slideshowSpeed: "3000",
-        transition: "fade" ,
-        opacity: ".2"
-    });
+    
+    
     
 });

9e62f76d536ac44bfa7c13b9b9d69cf0e6d75720
Matt Tilley <mtilley255@gmail.com>
2018-03-03 09:58:21 -0600

    Made another change to attempt tracking down problem.

--- primary.js
+++ primary.js
@@ -9,7 +9,7 @@ $(function()
     
     $("#BUT01").on("click", function()
     {
-        $(".SEC-T-01").animate({ "margin-left" : 50 });
+        $(".SEC-T-01").find(".BOX-T-01 : last").hide();
     });
     
     

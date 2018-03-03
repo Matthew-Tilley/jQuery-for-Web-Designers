32b8d1956fe3523d9a0788f484aabdffc3227fde
Matt Tilley <mtilley255@gmail.com>
2018-03-03 10:19:09 -0600

    Found problem / added comma - code working properly now.

--- primary.js
+++ primary.js
@@ -6,7 +6,7 @@ $(function()
     
     $("#BUT01").on("click", function()
     {
-       $(".BOX-T-01").animate({ "height" : "+=200%" 
+       $(".BOX-T-01").animate({ "height" : "+=200%", 
                                 "width" : "+=2000%"
                               });
         
